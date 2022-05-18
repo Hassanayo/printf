ddd28b31ff944ec6b25b02210369d51c6a9333b2
elisheba-beep <ekunife35@gmail.com>
2022-05-18 13:22:36 +0100

    created functions to print a character and a string

--- _printf.c
+++ _printf.c
@@ -5,14 +5,14 @@
 /**
  * _printf - prints out anything
  * @format: argument tpes that are passed to the function
+ * 
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
     va_list args;
     int n_displayed = 0;
-    int i = 0, k = 0;
-    char *str = NULL;
+    int i = 0;
 
     va_start(args, format);
 
@@ -28,26 +28,21 @@ int _printf(const char *format, ...)
             
             if (format[i+1] == 'c')
             {
-                _putchar(va_arg(args, int));
+                _print_char(args);
                 n_displayed++;
                 i++;
             }
             else if (format[i+1] == 's')
             {
                 i++;
-                str = va_arg(args, char *);
-                k = 0;
-                while (str[k] != '\0')
-                {
-                    _putchar(str[k]);
-                    n_displayed++;
-                    k++;
-                }
+                _print_str(args);
+                n_displayed++;
             }
-            else if (format[i+1 == '%'])
+            else if (format[i+1] == '%')
             {
                 i++;
                 _putchar('%');
+                n_displayed++;
             }
         }
 
