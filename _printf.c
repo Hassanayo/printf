7d81838285bfc433f00cd255aac1191f04b16f7f
Hassan Ayomide <funyem1710@gmail.com>
2022-05-18 11:26:55 +0100

    _printf

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _printf - prints out anything
+ * @format: argument tpes that are passed to the function
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    
+}
