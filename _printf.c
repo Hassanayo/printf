61193254ceb4dc0c1dc1425f2c125b056851b719
Hassan Ayomide <funyem1710@gmail.com>
2022-05-22 08:31:23 +0100

    flag test

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+
+unsigned char handle_flags(const char *flag, char *index);
 /**
  * check_for_specifiers - checks for valid format specifier
  * @format: possible format specifier
@@ -34,6 +36,48 @@ static int (*check_for_specifiers(const char *format))(va_list)
 	}
 	return (p[i].f);
 }
+/**
+ * handle_flags - Matches flags with corresponding values.
+ * @flag: A pointer to a potential string of flags.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If flag characters are matched - a corresponding value.
+ *         Otherwise - 0.
+ */
+unsigned char handle_flags(const char *flag, char *index)
+{
+	int i, j;
+	unsigned char ret = 0;
+	flag_t flags[] = {
+		{'+', PLUS},
+		{' ', SPACE},
+		{'#', HASH},
+		{'0', ZERO},
+		{'-', NEG},
+		{0, 0}
+	};
+
+	for (i = 0; flag[i]; i++)
+	{
+		for (j = 0; flags[j].flag != 0; j++)
+		{
+			if (flag[i] == flags[j].flag)
+			{
+				(*index)++;
+				if (ret == 0)
+					ret = flags[j].value;
+				else
+					ret |= flags[j].value;
+				break;
+			}
+		}
+		if (flags[j].value == 0)
+			break;
+	}
+
+	return (ret);
+}
+
 
 /**
  * _printf - prints anything
@@ -43,8 +87,10 @@ static int (*check_for_specifiers(const char *format))(va_list)
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0, count = 0;
+    char tmp;
+    unsigned char flags;
 	va_list valist;
-	int (*f)(va_list);
+	int (*f)(va_list, unsigned char);
 
 	if (format == NULL)
 		return (-1);
@@ -58,10 +104,12 @@ int _printf(const char *format, ...)
 		}
 		if (!format[i])
 			return (count);
+        tmp = 0;
+        flags = handle_flags(format + i + 1, &tmp);
 		f = check_for_specifiers(&format[i + 1]);
 		if (f != NULL)
 		{
-			count += f(valist);
+			count += f(valist, flags);
 			i += 2;
 			continue;
 		}
