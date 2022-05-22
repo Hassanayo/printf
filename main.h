968f82672041c8db6e0752d84d3c2c80b39990ef
elisheba-beep <ekunife35@gmail.com>
2022-05-22 07:57:11 +0200

    print hex function created and main.h modified

--- main.h
+++ main.h
@@ -16,6 +16,8 @@ typedef struct print
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_hex(unsigned int n, unsigned int c);
+static unsigned long _pow(unsigned int base, unsigned int exponent);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
@@ -23,5 +25,8 @@ int print_d(va_list d);
 int print_o(va_list o);
 int print_b(va_list b);
 int print_u(va_list u);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
 
 #endif /* _MAIN_H_ */
