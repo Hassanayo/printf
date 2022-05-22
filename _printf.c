8d4745271fbca98a3a67e16a8a2a67f9654b6085
Hassan Ayomide <funyem1710@gmail.com>
2022-05-22 06:23:41 +0100

    change

--- _printf.c
+++ _printf.c
@@ -3,9 +3,7 @@
 /**
  * check_for_specifiers - checks for valid format specifier
  * @format: possible format specifier
- * 
  * Return: pointer to valid function or NULL
- * 
  */
 static int (*check_for_specifiers(const char *format))(va_list)
 {
@@ -16,65 +14,54 @@ static int (*check_for_specifiers(const char *format))(va_list)
     };
 
     for (i = 0; p[i].t != NULL; i++)
-    {
-        if (*(p[i].t) == *format)
-        {
-            break;
-        }
-    }
-    return (p[i].f);
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
 }
 
 /**
  * _printf - prints anything
  * @format: arguments passed to the function
- * 
  * Return: number of chars printed
  */
 int _printf(const char *format, ...)
 {
-    unsigned int i = 0, count = 0;
-    va_list valist;
-    int (*f)(va_list);
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-    if (format == NULL)
-    {
-        return (-1);
-    }
-    va_start(valist, format);
-    while (format[i])
-    {
-        for (; format[i] != '%' && format[i]; i++)
-        {
-            _putchar(format[i]);
-            count++;
-        }
-        if (!format[i])
-        {
-            return (count);
-        }
-        f = check_for_specifiers(&format[i + 1]);
-        if (f != NULL)
-        {
-            count += f(valist);
-            i += 2;
-            continue;
-        }
-        if (!format[i + 1])
-        {
-            return (-1);
-        }
-        _putchar(format[i]);
-        count++;
-        if (format[i + 1] == '%')
-        {
-            i += 2;
-        }
-        else
-        {
-            i++;
-        }
-    }
-    va_end(valist);
-    return (count);
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
 }
